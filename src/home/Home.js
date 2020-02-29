41da161b11575e2dfebd2de690cbef984cb9b39d
DanNelson9999 <dan.nelson.fred@gmail.com>
2020-02-29 15:00:35 -0400

    example

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 
+const temp = ["Dan", "Sam", "Nancy"];
+
+const func = (array) => {
+    return array.map( result => {
+        return (
+             <span>{ result + " "}</span>
+        )
+    })
+}
+
 const Home = (props) => {
     return (
         <>
             <div>
                 Home
             </div>
+            { func(temp)}
         </>
     )
 }
